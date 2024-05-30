6083d4dd5399768befa02e9225e632c53c59d054
notPHPuser <artem2005dm@mail.ru>
2024-05-31 01:06:36 +0300

    w
    w

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <>sgfdsg</>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import Login from './pages/Log/Login';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
+  </Router>,
+  document.getElementById('root'),
+);
