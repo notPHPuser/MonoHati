c7a7bded804dcf1c679f997ced463de0f215bab1
timch12 <qwerty1234567890zero@gmail.com>
2024-06-05 17:28:43 +0300

    тр

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,8 +14,7 @@ function Header({ login = 'login' }: header) {
             {login}
           </Link>
         </div>
-      </div>
-      <p>sperm123123123a</p>
+      </div>   
     </>
   );
 }
