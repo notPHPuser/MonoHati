dd1a7563bc8c184221b0ca0af80c70e088b7691a
notPHPuser <artem2005dm@mail.ru>
2024-06-07 23:54:01 +0300

    hrhr

--- client/src/pages/Log/Login.tsx
+++ client/src/pages/Log/Login.tsx
@@ -1,10 +1,23 @@
 // import { Link } from 'react-router-dom';
 import './Login.css';
+import { User } from '../../interfaces/infterface';
 
 function Login() {
+  const user: User = {
+    name: 'Имя',
+    email: 'Почта',
+    password: 'Пароль',
+  };
   return (
     <>
-      <div className='register'></div>
+      <div className='register'>
+        <p className='enterReg'>Вход</p>
+        <form className='formForReg'>
+          <input type='text' value={user.name} />
+          <input type='text' value={user.email} />
+          <input type='text' value={user.password} />
+        </form>
+      </div>
     </>
   );
 }
